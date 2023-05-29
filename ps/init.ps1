ff8eaf97656e6dffd4ab1da5fd82fe813d9e2672
cke <cke@artificialcore.com>
2023-05-29 09:22:28 +0200

    Added more commands to my powershell script

--- ps/init.ps1
+++ ps/init.ps1
@@ -5,6 +5,13 @@ echo "Welcome to Carlos's PowerShell setup"
 oh-my-posh init pwsh | Invoke-Expression
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/honukai.omp.json" | Invoke-Expression
 
+function Set-TabHeader {
+    param(
+        [string]$Header
+    )
+    $Host.UI.RawUI.WindowTitle = $Header 
+}
+
 # Navigation functions
 
 function Projects {
@@ -15,5 +22,35 @@ function Local {
 }
 function Ts {
     Set-Location -Path "D:\Projects\Training\game_server" -ErrorAction Stop
-    Invoke-Expression "neovide"
+    Set-TabHeader -Header "game_server"
+    Invoke-Expression "nvim"
+}
+function Tb {
+    Set-Location -Path "D:\Projects\Training\bot_server" -ErrorAction Stop
+    Set-TabHeader -Header "bot_server"
+    Invoke-Expression "nvim"
+}
+function Tu {
+    Set-Location -Path "D:\Projects\Training\GameArchitecture" -ErrorAction Stop
+    Set-TabHeader -Header "Unity"
+    Invoke-Expression "nvim"
+}
+function Tc {
+    $te = "wt.exe"  # Replace with the path to your Windows Terminal executable if necessary
+
+    $g = "D:\Projects\Training\game_server"
+    $b = "D:\Projects\Training\bot_server"
+
+    # Start the first terminal with the game server path
+    Start-Process -FilePath $te -ArgumentList "-w 0 -p `"Windows PowerShell`" -d `"$g`"; split-pane --vertical -p `"Windows PowerShell`" -d `"$b`" "
+}
+function T {
+    $t = "wt.exe"  # Replace with the path to your Windows Terminal executable if necessary
+
+    $g = "D:\Projects\Training\game_server"
+    $b = "D:\Projects\Training\bot_server"
+
+    Start-Process -FilePath $t -ArgumentList "-w 0 -p `"Windows PowerShell`" powershell -noExit `"Tb`" "
+    Invoke-Expression "Tc"
+    Invoke-Expression "Ts"
 }
